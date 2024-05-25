fb1e46616df28082f71f83375a0c9d7608f28a9e
OussemaChercheri <oussama.chercheri@tek-up.de>
2024-05-25 01:14:55 +0100

    add footer

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
-
+import Hero from "@/components/Hero";
+import Footer from "../components/footer";
 type Props = {
   children: React.ReactNode;
   showHero?: boolean;
@@ -8,11 +9,12 @@ type Props = {
 const Layout = ({ children }: Props) => {
   return (
     <div className="felx-col min-h-screen">
-      <Header/>
-
+      <Header />
+      <Hero />
       <div className="container mx-auto flex-1 py-10">{children}</div>
+      <Footer />
     </div>
-    
+
   );
 };
 
